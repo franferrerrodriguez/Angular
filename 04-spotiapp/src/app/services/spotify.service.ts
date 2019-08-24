a005a85c8775a80aff5eb7002ea812b2d2dde1ae
alex1ferrer <alejandroferrerrodriguezz@gmail.com>
2019-08-24 20:25:56 +0200

    g

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -7,34 +7,36 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
 
-  private token:string = "BQCMqe4HrOklKRrpKE7YN9raj5BgnRxL1He4f0NblBunTKKCnA3fKnA6IBxmNjMF3-td-ZIEqyIW3dFe9dw";
-
   constructor( private http:HttpClient ) { 
     console.log("Servicio Spotify Listo");
   }
 
-  getNewReleases():any{
+  getQuery(query:string){
+
+    const token:string = "BQAxx1EJtDpQTu937C5GHmaRc2cpIQw1ZhJwARIpF7mU_frDwK0HO_plVvtB_GLlbx-BxO90GcpXp0G3al0";
+    const URI = `https://api.spotify.com/v1/${ query }`;
+
     let headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.token
+      'Authorization': 'Bearer ' + token
     });
 
-    return this.http.get(
-      'https://api.spotify.com/v1/browse/new-releases?country=ES&limit=40',
-      { headers }
-    ).pipe(map( (data:any) => {
+    return this.http.get(URI, { headers })
+
+  }
+
+  getNewReleases():any{
+
+    return this.getQuery("browse/new-releases?country=ES&limit=40").pipe(map( (data:any) => {
       return data.albums.items;
     } ));
   }
 
   search(q:string):any{
-    let headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.token
-    });
 
-    return this.http.get(
-      `https://api.spotify.com/v1/search?q=${ q }&type=artist&limit=10&offset=5`,
-      { headers }
-    ).pipe(map( (data:any) => data.artists.items ) );
+    return this.getQuery(`search?q=${ q }&type=artist&limit=10&offset=5`).pipe(map( (data:any) => {
+      return data.artists.items;
+    } ));
+
   }
 
 }
